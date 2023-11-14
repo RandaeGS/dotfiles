4d2d3defe345e92c643653ff4652f47f1a49691f
Randae <garciasanchezrd@gmail.com>
2023-11-14 17:42:15 -0400

    Adding rofi and modifying things

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,5 +3,4 @@
 xrandr --output HDMI-0 --mode 1920x1080 --rate 75 --pos 0x0 --rotate normal --output DP-0 --primary --mode 1920x1080 --rate 165 --pos 1920x0 --rotate normal 
 
 # Random wallpaper #
-feh --bg-fill --randomize ~/Pictures/Wallpapers/*
-
+wal -s -i ~/Pictures/Wallpapers
--- qtile/config.py
+++ qtile/config.py
@@ -1,8 +1,8 @@
 # Imports #
 import os
 import subprocess
-from libqtile import qtile
-from libqtile import bar, layout, widget
+import json
+from libqtile import qtile, bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
@@ -12,8 +12,10 @@ terminal = "kitty"
 browser = "firefox"
 
 keys = [
-   # A list of available commands that can be bound to keys can be found
-   # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+
+   # Switch between screens
+   Key([mod], "period", lazy.next_screen(), desc="Move to the next screen"),
+
    # Switch between windows
    Key([mod], "j", lazy.layout.left(), desc="Move focus to left"),
    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
@@ -26,40 +28,24 @@ keys = [
    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
    Key([mod, "shift"], "k", lazy.layout.shuffle_down(), desc="Move window down"),
    Key([mod, "shift"], "i", lazy.layout.shuffle_up(), desc="Move window up"),
-   # Grow windows. If current window is on the edge of screen and direction
-   # will be to screen edge - window would shrink.
-   Key([mod, "control"], "j", lazy.layout.grow_left(), desc="Grow window to the left"),
-   Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-   Key([mod, "control"], "k", lazy.layout.grow_down(), desc="Grow window down"),
-   Key([mod, "control"], "i", lazy.layout.grow_up(), desc="Grow window up"),
-   Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-   # Toggle between split and unsplit sides of stack.
-   # Split = all windows displayed
-   # Unsplit = 1 window displayed, like Max layout, but still with
-   # multiple stack panes
-   Key(
-      [mod, "shift"],
-      "Return",
-      lazy.layout.toggle_split(),
-      desc="Toggle between split and unsplit sides of stack",
-   ),
+
    # Toggle between different layouts as defined below
    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-   Key(
-      [mod],
-      "f",
-      lazy.window.toggle_fullscreen(),
-      desc="Toggle fullscreen on the focused window",
-   ),
-   Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+   Key([mod],"f",lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window",),
    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-   Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+
+   # Volume control #
+   Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Raise volume"),
+   Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Lower volume"),
+   Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Toggle volume"),
+
    # Applications Shortcuts # 
-   Key([mod], "p", lazy.spawn(terminal), desc="Launch kitty terminal"),
+   Key([mod], "Return", lazy.spawn(terminal), desc="Launch kitty terminal"),
    Key([mod], "b", lazy.spawn(browser), desc="Launch firefox"),
-   Key([mod], "x", lazy.spawn("krusader"), desc="Launch krusader"),
+   Key([mod], "x", lazy.spawn("kitty ranger"), desc="Launch ranger"),
+   Key([mod], "Backspace", lazy.spawn("rofi -show combi"), desc="Lauch Rofi"),
 ]
 
 # Groups #
@@ -79,100 +65,175 @@ for i in groups:
             Key(
                [mod, "shift"],
                i.name,
-               lazy.window.togroup(i.name, switch_group=True),
-               desc="Switch to & move focused window to group {}".format(i.name),
+               lazy.window.togroup(i.name, switch_group=False),
+               desc="Switch window to group {}".format(i.name),
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
 
 # Layouts #
 layout_theme = {
    "border_width": 3,
-   "margin": 8,
+   "margin": 6,
    "border_focus": "#28439e"
 }
 
 layouts = [
-   # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
    layout.MonadTall(**layout_theme),
    layout.Max()
-   # Try more layouts by unleashing below layouts.
-   # layout.Stack(num_stacks=2),
-   # layout.Bsp(),
-   # layout.Matrix(),
-   # layout.MonadWide(),
-   # layout.RatioTile(),
-   # layout.Tile(),
-   # layout.TreeTab(),
-   # layout.VerticalTile(),
-   # layout.Zoomy(),
 ]
 
+# Colors #
+#Pywal Colors
+colors = os.path.expanduser('~/.cache/wal/colors.json')
+colordict = json.load(open(colors))
+ColorZ=(colordict['colors']['color0'])
+ColorA=(colordict['colors']['color1'])
+ColorB=(colordict['colors']['color2'])
+ColorC=(colordict['colors']['color3'])
+ColorD=(colordict['colors']['color4'])
+ColorE=(colordict['colors']['color5'])
+ColorF=(colordict['colors']['color6'])
+ColorG=(colordict['colors']['color7'])
+ColorH=(colordict['colors']['color8'])
+ColorI=(colordict['colors']['color9'])
+
+# Bar #
 widget_defaults = dict(
-   font="sans",
+   font="JetBrainsMono Nerd Font medium",
    fontsize=12,
    padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
-screens = [
-    Screen(
-        top=bar.Bar(
-            [
-               widget.GroupBox(),
-               widget.Prompt(),
-               widget.WindowName(),
-               widget.CurrentLayout(),
-               widget.Chord(
-                  chords_colors={
-                     "launch": ("#ff0000", "#ffffff"),
-                  },
-                  name_transform=lambda name: name.upper(),
-               ),
-               widget.TextBox("default config", name="default"),
-               widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-               # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-               # widget.StatusNotifier(),
-               widget.Systray(),
-               widget.Clock(format="%d-%m %A %H:%M"),
-               widget.QuickExit(),
-               
-            ],
-            24,
-            wallpaper='/home/randaegs/Pictures/Wallpapers/zfirvnutwp2a1.jpg',
-            wallpaper_mode='fit'
-            
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
-      ),
-   Screen(
-       top=bar.Bar(
-            [
-               widget.GroupBox(),
-               widget.WindowTabs(),
-               widget.CurrentLayout(),
-            ],
-         24,
-         wallpaper='~/Pictures/Wallpapers/wallpaperflare.com_wallpaper(2).jpg',
-         wallpaper_mode='fit'
-       )
-   )
-]
+def volumeControl():
+   qtile.cmd_spawn("pavucontrol")
 
-# Drag floating layouts.
-mouse = [
-   Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-   Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-   Click([mod], "Button2", lazy.window.bring_to_front()),
+def toggleVolume():
+   lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle", desc="Toggle audio")
+
+screens = [
+	Screen(
+		top=bar.Bar(
+			[
+			widget.GroupBox(highlight_method="block"),
+			widget.WindowTabs(),
+         widget.TextBox(
+            text='',
+            background="#232136", #Va del color de la barra
+            foreground="#f6c177",
+            padding=0,
+            fontsize=42
+         ),
+			widget.TextBox(
+            text=' ',
+            background="#f6c177",
+            foreground="#191724",
+            padding=7
+         ),
+         widget.CurrentLayout(
+            background="#f6c177",
+            foreground="#191724"
+         ),
+         widget.TextBox(
+            text='',
+            background="#f6c177",
+            foreground="#9ccfd8",
+            padding=0,
+            fontsize=42
+         ),
+         widget.Volume(
+            background="#9ccfd8",
+            foreground="191724",
+            mouse_callbacks = {"Button1": volumeControl, "Button2": toggleVolume}
+         ),
+         widget.TextBox(
+            text='',
+            background="#9ccfd8",
+            foreground="#edf2fa",
+            padding=0,
+            fontsize=42
+         ),
+			widget.Clock(
+            background="#edf2fa",
+            foreground="#191724",
+            format="🕗 %H:%M - %d/%m %a",
+            update_interval = 60.0
+         ),
+			],
+			25,
+         background="#232136",
+		),
+		# x11_drag_polling_rate = 60,
+	),
+	Screen(
+		top=bar.Bar(
+			[
+			widget.GroupBox(highlight_method="block"),
+			widget.WindowTabs(),
+			widget.Systray(),
+         widget.TextBox(
+            text='',
+            background="#232136", #Va del color de la barra
+            foreground="#f6c177",
+            padding=0,
+            fontsize=42
+         ),
+			widget.TextBox(
+            text=' ',
+            background="#f6c177",
+            foreground="#191724",
+            padding=7
+         ),
+         widget.CurrentLayout(
+            background="#f6c177",
+            foreground="#191724"
+         ),
+         widget.TextBox(
+            text='',
+            background="#f6c177",
+            foreground="#9ccfd8",
+            padding=0,
+            fontsize=42
+         ),
+			widget.Notify(default_timeout=5),
+         widget.Volume(
+            background="#9ccfd8",
+            foreground="191724",
+            fmt=" {}",
+            mouse_callbacks = {"Button1": volumeControl, "Button2": toggleVolume}
+         ),
+         widget.TextBox(
+            text='',
+            foreground="#eb6f92",
+            background="#9ccfd8",
+            padding=0,
+            fontsize=42
+         ),
+         widget.Memory(
+            format="{MemUsed: .0f}{mm}",
+            background="#eb6f92",
+            foreground="#191724",
+            interval=1.0
+         ),
+         widget.TextBox(
+            text='',
+            background="#eb6f92",
+            foreground="#edf2fa",
+            padding=0,
+            fontsize=42
+         ),
+			widget.Clock(
+            background="#edf2fa",
+            foreground="#191724",
+            format="\u23F1 %A %d/%m %H:%M",
+            update_interval = 60.0
+         )
+			],
+			25,
+         background="#232136",
+		)
+   )
 ]
 
 dgroups_key_binder = None
@@ -180,7 +241,7 @@ dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
-cursor_warp = False
+cursor_warp = True # Cursor follows movement #
 floating_layout = layout.Floating(
    float_rules=[
       # Run the utility of `xprop` to see the wm class and name of an X client.
@@ -203,6 +264,12 @@ auto_minimize = True
 
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
+
+# Startup don't forget to chmod +x autostart.h#
+@hook.subscribe.startup_once
+def autostart():
+   home = os.path.expanduser('~/.config/qtile/autostart.sh')
+   subprocess.run([home])
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
